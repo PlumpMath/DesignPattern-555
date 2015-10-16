f03b215b99634f5ee1194437c24b7796e82d59ad
angeltsh <ywy0719@163.com>
2015-10-16 16:06:50 +0800

    装饰者更新

---
+++ src/Decorator/demo2/InputTest.java
@@ -0,0 +1,21 @@
+package Decorator.demo2;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class InputTest {
+	public static void main(String[] args) {
+		int c;
+		try {
+			InputStream in = new LowerCaseInputStream(new BufferedInputStream(
+					System.in));
+			while ((c = in.read()) >= 0) {
+				System.out.print((char) c);
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
